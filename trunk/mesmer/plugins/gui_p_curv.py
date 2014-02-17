9c2708d80295c59b8f5d03cffe64c59373b3722a
elihuihms <elihuihms@b5e34e47-6b48-2bd4-98de-ee3c27b9493a>
2014-02-17 16:21:55 +0000

    Fix to bug in curv_plot GUI plugin

--- trunk/mesmer/plugins/gui_p_curv.py
+++ trunk/mesmer/plugins/gui_p_curv.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import argparse
 
 from subprocess				import Popen
@@ -15,7 +16,7 @@ class plugin(guiPlotPlugin):
 			'CURV','CURV0','CURV1','CURV2','CURV3','CURV4','CURV5','CURV6','CURV7','CURV8','CURV9',
 			'DEER','DEER0','DEER1','DEER2','DEER3','DEER4','DEER5','DEER6','DEER7','DEER8','DEER9')
 		self.parser = None
-	
+
 		# check the script local to the installation first, otherwise use what's in the system's path
 		self.exe = os.path.join(os.path.dirname(os.path.dirname(__file__)),'utilities','make_curv_plot.py')
 		if( not os.access(self.exe, os.R_OK) ):
